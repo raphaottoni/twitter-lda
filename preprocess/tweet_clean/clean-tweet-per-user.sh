ef08e649a97daf7dd37a5071e22aee0a74d44794
raphaottoni <rapha.ottoni@gmail.com>
2013-08-14 19:58:08 -0400

    adicionando preprocessamento de limpar tweet

---
+++ preprocess/tweet_clean/clean-tweet-per-user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while getopts 'i:o:' opt;  do
+  case $opt in
+    i)
+        input=$OPTARG
+        ;;
+    o)
+        output=$OPTARG
+        ;;
+  esac
+done
+
+for file in $(ls $input)
+do
+  zcat $input/$file | awk '{print tolower($0)}' | sed -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g;' | sed  -e "s/[^a-zA-Z0-9#@ ]//g"  | gzip > $output/$file
+done
